2440ce59ee9a8cc8924388fd6efc27950ef9e5d5
Sagar Koshti <sagarkoshti.au@gmail.com>
2016-03-10 12:11:29 +0530

    Coursera: R --> Week 3 Assignment
    Function to calculate the Matrix Inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Function to return the List of Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+	##### Set inverse to NULL
+	cachInv <- NULL 
+  
+	##### Set input value to z
+	set <- function(vInput = matrix()) {
+		z <<- vInput 
+		##### Set inverse to NULL if set already
+		cachInv <<- NULL
+	}
+	
+	##### get
+	get <- function() z
+  
+	##### Set inverse variable 
+	setinverse <- function(inverseVal) {
+		cachInv <<- inverseVal 
+		##### Return the inverse
+		return(cachInv)
+	}
+  
+	##### getinverse
+	getinverse  <- function() cachInv
+	
+	list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## Function to calculate Inverse of Matrix using solve()
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+	##### Check calculated inverse
+	calculatedInv <- x$getinverse() 
+  
+	##### Check for matrix and values
+	if(!is.null(calculatedInv) && is.matrix(calculatedInv)) { 
+		message("Cached data found:")
+		return(calculatedInv)
+	}
+  
+	##### Get the matrix
+	matrixToSolve <- x$get()  
+  
+	##### Solve function
+	calculatedInv <- solve(matrixToSolve)
+  
+	##### Set inverse
+	message("Matrix Inverse:  ") 
+	x$setinverse(calculatedInv)
 }
